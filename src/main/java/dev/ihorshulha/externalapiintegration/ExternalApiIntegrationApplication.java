e87fb6cff7ccfebd2271aa11e223c04fd59b9ee0
ihorshulha <igor.shulga8@gmail.com>
2025-07-29 09:23:17 +0200

    initial commit

---
+++ src/main/java/dev/ihorshulha/externalapiintegration/ExternalApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package dev.ihorshulha.externalapiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ihorshulha/externalapiintegration/ExternalApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ihorshulha.externalapiintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
